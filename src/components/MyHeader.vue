bdb9dbb26ab593affbd2d1ead8c32fafb2f3b7ee
koheitanaka <sn0w.tanakoh@gmail.com>
2022-05-18 21:54:13 +0900

    更新文言追加

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="navbar navbar-light" style="background-color: gold;">
-        <h2 class="navbar-brand">田中タスク_編集中</h2>
+        <h2 class="navbar-brand">田中タスク_編集中（最終更新日：2022/5/18）</h2>
     </nav>
 </template>
 <script>
